f33c0b66682deb9025e583aa5d32bad943de9ea7
Hem Pun <hembdrpun10@gmail.com>
2024-03-19 19:40:58 +0545

    Added:file upload

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localfilePath: string) => {
+  try {
+    if (!localfilePath) return null;
+    //upload file on cloudinary
+    const res = await cloudinary.uploader.upload(localfilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("File uploaded successfully", res.url);
+    return res;
+  } catch (error) {}
+};
