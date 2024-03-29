467ef218466b288525cb5e01a86fe73f64bcbe6f
Hem Pun <hembdrpun10@gmail.com>
2024-03-29 20:12:57 +0545

    Added:Routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateAvtar,
+  updateUserCoverImage,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -31,9 +35,23 @@ router.post(
 router.post("/login", loginUser);
 
 //Secured Roues
+
 // /api/v1/user/logout
 router.post("/logout", verifyJWT, logoutUser);
 
+// /api/v1/user/refresh-access-token
 router.post("/refresh-access-token", refreshAccessToken);
+
+// /api/v1/user/change-password
+router.post("/change-password", verifyJWT, changeCurrentPassword);
+
+// /api/v1/user/update-account-details
+router.post("/update-account-details", verifyJWT, updateAccountDetails);
+
+// /api/v1/user/update-avatar
+router.post("/update-avatar", upload.single, updateAvtar);
+
+// /api/v1/user/update-cover-image
+router.post("/update-cover-image", upload.single, updateUserCoverImage);
 
 export default router;
